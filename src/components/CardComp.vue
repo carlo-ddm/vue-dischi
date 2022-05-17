157eea43e205208307d8a97665882e522bc321b3
carlo-ddm <carloduminuco90@gmail.com>
2022-05-17 18:32:06 +0200

    preparazione html css main! vedere chiamata axios

---
+++ src/components/CardComp.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="cd-container d-flex justify-content-between flex-wrap">
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+  <div class="cd-card m-2"></div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'CardComp'
+}
+</script>
+
+<style lang="scss" scoped>
+.cd-card {
+  flex-basis: calc(100% / 6);
+  height: 300px;
+  border: 1px solid white;
+}
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,21 +1,35 @@
 <template>
-  <main>
+  <main class="d-flex justify-content-center align-items-center">
     <div class="container">
-      Main
+      <CardComp />
     </div>
   </main>
 </template>
 
 <script>
+// import '../assets/data/chiamata/'
+import CardComp from './CardComp.vue'
 export default {
-  name: 'MainComp'
+    name: "MainComp",
+    // data() {
+    //     return {
+    //         chiamata
+    //     };
+    // },
+    components: { CardComp }
 }
+
 </script>
 
 <style lang="scss" scoped>
 main {
   height: 94vh;
   background-color: #17212C;
+
+  .container {
+    border: 2px solid white;
+    height: 650px; //da de bug
+  }
 }
 
 </style>
